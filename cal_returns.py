57768918fe7fc4f0509fdae982368dcbfa8e7611
Anish J <anishjeyakumar7@gmail.com>
2024-06-02 20:09:43 +0530

    Update cal_returns.py

--- cal_returns.py
+++ cal_returns.py
@@ -1,1 +1,16 @@
+initial_cash = 100000
+position_size = 1000
 
+cash = initial_cash
+positions = 0
+
+for i in range(len(signals)):
+    if signals[i] == 1:
+        positions += position_size
+        cash -= data['GBPUSD=X'][i] * position_size
+    elif signals[i] == -1:
+        positions -= position_size
+        cash += data['GBPUSD=X'][i] * position_size
+
+final_value = cash + positions * data['GBPUSD=X'].iloc[-1]
+print(f'Final portfolio value: {final_value}')
